2d239a92bf00bdeccb20755d19bcf9064b1e9d9f
TrollsterCooleg <git@cooleg.me>
2023-12-01 17:23:42 -0600

    Make sure that server has boat collisions disabled for noxesium utils integration

--- BoatHiderMain/src/main/java/me/cooleg/boathider/NoxesiumUtilsHook.java
+++ BoatHiderMain/src/main/java/me/cooleg/boathider/NoxesiumUtilsHook.java
@@ -1,10 +1,14 @@
 package me.cooleg.boathider;
 
+import com.noxcrew.noxesium.api.protocol.rule.ServerRuleIndices;
 import me.superneon4ik.noxesiumutils.NoxesiumUtils;
 import me.superneon4ik.noxesiumutils.events.NoxesiumPlayerJoinEvent;
+import me.superneon4ik.noxesiumutils.network.clientbound.ClientboundChangeServerRulesPacket;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.List;
 
 public class NoxesiumUtilsHook implements Listener {
 
@@ -18,6 +22,12 @@ public class NoxesiumUtilsHook implements Listener {
     public void noxesiumLoginEvent(NoxesiumPlayerJoinEvent event) {
         if (event.getProtocolVersion() < 4) {return;}
         listeners.showAll(event.getPlayer());
+
+        var rule = NoxesiumUtils.getManager().getServerRule(event.getPlayer(), ServerRuleIndices.DISABLE_BOAT_COLLISIONS);
+        if (rule != null) {
+            rule.setValue(true);
+            new ClientboundChangeServerRulesPacket(List.of(rule)).send(event.getPlayer());
+        }
     }
 
     public boolean canShowSafely(Player player) {
